0d9aa64d245c62c497c84a47f9053c33307c1164
YuraBobeshko <gorobech55@gmail.com>
2020-01-13 18:22:49 +0200

    sol

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React from 'react';
+import React from "react";
 import { Provider } from "react-redux";
-import ScrollUpButton from 'react-scroll-up-button';
+import ScrollUpButton from "react-scroll-up-button";
 import { store } from "./store";
-import './App.scss';
+import "./App.scss";
 
-import Header from './components/Header/Header'
-import { Sidebar } from './components/Sidebar/index'
-import { ListCocktail } from './components/ListCocktail/index'
+import Header from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/index";
+import { ListCocktail } from "./components/ListCocktail/index";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <Header />
-        <main className='main'>
+        <main className="main">
           <Sidebar />
           <ListCocktail />
         </main>
         <ScrollUpButton
-        StopPosition={0}
-        ShowAtPosition={150}
-        EasingType="easeOutCubic"
-        AnimationDuration={200}
-        ContainerClassName="ScrollUpButton__Container"
-        TransitionClassName="ScrollUpButton__Toggled"
-        style={{
-          width: "36px",
-          height: "36px",
-          backgroundColor: "#59BD5A",
-          outline: "none",
-          borderRadius: "18px",
-          boxShadow: "0 2px 4px 0 #9b9b9b"
-        }}
-      />
+          StopPosition={0}
+          ShowAtPosition={150}
+          EasingType="easeOutCubic"
+          AnimationDuration={200}
+          ContainerClassName="ScrollUpButton__Container"
+          TransitionClassName="ScrollUpButton__Toggled"
+          style={{
+            width: "36px",
+            height: "36px",
+            backgroundColor: "#59BD5A",
+            outline: "none",
+            borderRadius: "18px",
+            boxShadow: "0 2px 4px 0 #9b9b9b"
+          }}
+        />
       </Provider>
     </div>
   );
